3573a62e34928d7325a09e039e84564bac0a0064
Mike Chastain & Charles Moss <student@epicodus.com>
2015-09-16 11:39:54 -0700

    add passing tests for player, game, and joinGame

--- js/main.js
+++ js/main.js
@@ -2,10 +2,13 @@ function Player(name){
   this.name = name;
   this.score = 0;
 
-
-
 }
 
-Player.prototype.joinGame = function(){
+Player.prototype.joinGame = function(gameTojoin){
+    gameTojoin.players.push(this);
+}
 
+function Game(){
+    this.players = [];
+    this.maxPlayers = 2;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,45 @@
 describe('Player', function() {
-  it("it returns a player object with name and score of 0", function() {
-    var testPlayer = new Player("Mad Max");
 
-    expect(testPlayer.name).to.equal("Mad Max");
-    expect(testPlayer.score).to.equal(0);
+    it("returns a player object with name and score of 0", function() {
+        var testPlayer = new Player("Mad Max");
 
-  });
+        expect(testPlayer.name).to.equal("Mad Max");
+        expect(testPlayer.score).to.equal(0);
+
+    });
+
+    it("joins a player to a game", function() {
+        var testPlayer = new Player("Mad Max");
+        var testGame = new Game();
+        testPlayer.joinGame(testGame);
+
+        expect(testGame.players[0]).to.equal(testPlayer);
+
+    });
+
+    it("tests that 2 players equals max players", function() {
+        var testPlayer = new Player("Mad Max");
+        var testPlayer2 = new Player("Rad Rex");
+        var testGame = new Game();
+        testPlayer.joinGame(testGame);
+        testPlayer2.joinGame(testGame);
+
+        expect(testGame.players[1]).to.equal(testPlayer2);
+        expect(testGame.players.length).to.equal(testGame.maxPlayers);
+
+    });
+
+
+
+
+});
+
+describe('Game', function() {
+    it("it returns a game object with an empty array", function() {
+        var testGame = new Game();
+
+        expect(testGame.players).to.eql([]);
+
+    });
+
 });
